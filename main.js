dea761ea92e16214d1548ed86beb82e1a8ec46a3
frankirito <francoishenri.dupuich@gmail.com>
2016-08-21 16:31:28 +0000

    dot nav

--- main.js
+++ main.js
@@ -1,3 +1,53 @@
+jQuery(document).ready(function($){
+	var contentSections = $('.cd-section'),
+		navigationItems = $('#cd-vertical-nav a');
+
+	updateNavigation();
+	$(window).on('scroll', function(){
+		updateNavigation();
+	});
+
+	//smooth scroll to the section
+	navigationItems.on('click', function(event){
+        event.preventDefault();
+        smoothScroll($(this.hash));
+    });
+    //smooth scroll to second section
+    $('.cd-scroll-down').on('click', function(event){
+        event.preventDefault();
+        smoothScroll($(this.hash));
+    });
+
+    //open-close navigation on touch devices
+    $('.touch .cd-nav-trigger').on('click', function(){
+    	$('.touch #cd-vertical-nav').toggleClass('open');
+  
+    });
+    //close navigation on touch devices when selectin an elemnt from the list
+    $('.touch #cd-vertical-nav a').on('click', function(){
+    	$('.touch #cd-vertical-nav').removeClass('open');
+    });
+
+	function updateNavigation() {
+		contentSections.each(function(){
+			$this = $(this);
+			var activeSection = $('#cd-vertical-nav a[href="#'+$this.attr('id')+'"]').data('number') - 1;
+			if ( ( $this.offset().top - $(window).height()/2 < $(window).scrollTop() ) && ( $this.offset().top + $this.height() - $(window).height()/2 > $(window).scrollTop() ) ) {
+				navigationItems.eq(activeSection).addClass('is-selected');
+			}else {
+				navigationItems.eq(activeSection).removeClass('is-selected');
+			}
+		});
+	}
+
+	function smoothScroll(target) {
+        $('body,html').animate(
+        	{'scrollTop':target.offset().top},
+        	600
+        );
+	}
+});
+
 new Vivus('keyhole',  {
     type: 'scenario',
     duration:50,
@@ -122,25 +172,3 @@ function readCookie(name) {
   return null;
 }
 
-//scrollreaveal
-window.sr = ScrollReveal({ duration: 2000 });;
-
-sr.reveal('.foo', {     
-    duration: 400,
-    delay: 100,
-    opacity: 0 }, 50);
-
-sr.reveal('.folio', {
-    duration: 900,
-    delay: 0,
-    opacity: 0.6
-});
-sr.reveal('.foliotwo', {
-    duration: 500,
-    delay: 0,
-    opacity: 0.6
-});
-sr.reveal('.aproposmooth', {
-    delay: 0,
-    opacity: 0.8
-});
