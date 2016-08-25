a6301b5ea677880fd7b2ec6b6845283dc0ec06bb
frankirito <francoishenri.dupuich@gmail.com>
2016-08-25 07:02:27 +0000

    fancy header

--- main.js
+++ main.js
@@ -1,5 +1,41 @@
+// Hide Header on on scroll down
+var didScroll;
+var lastScrollTop = 0;
+var delta = 5;
+var navbarHeight = $('header').outerHeight();
 
+$(window).scroll(function(event){
+    didScroll = true;
+});
 
+setInterval(function() {
+    if (didScroll) {
+        hasScrolled();
+        didScroll = false;
+    }
+}, 250);
+
+function hasScrolled() {
+    var st = $(this).scrollTop();
+    
+    // Make sure they scroll more than delta
+    if(Math.abs(lastScrollTop - st) <= delta)
+        return;
+    
+    // If they scrolled down and are past the navbar, add class .nav-up.
+    // This is necessary so you never see what is "behind" the navbar.
+    if (st > lastScrollTop && st > navbarHeight){
+        // Scroll Down
+        $('header').removeClass('nav-down').addClass('nav-up');
+    } else {
+        // Scroll Up
+        if(st + $(window).height() < $(document).height()) {
+            $('header').removeClass('nav-up').addClass('nav-down');
+        }
+    }
+    
+    lastScrollTop = st;
+}
 
 jQuery(document).ready(function($){
 	var contentSections = $('.cd-section'),
@@ -86,20 +122,7 @@ $('body').flowtype({
    maxFont   : 30,
    fontRatio : 30
 });
-//langage switch
-var elem = document.querySelector('.js-switch');
-var init = new Switchery(elem); // put option after elem attribute
-defaults = {
-    color             : '#64bd63'
-  , secondaryColor    : '#dfdfdf'
-  , jackColor         : '#fff'
-  , jackSecondaryColor: null
-  , className         : 'switchery'
-  , disabled          : false
-  , disabledOpacity   : 0.5
-  , speed             : '0.1s'
-  , size              : 'default'
-}
+
 
 //langage select 
    var languageSelect = document.getElementById('LanguageSelect');
